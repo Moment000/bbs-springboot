4e229559a8157d38cbce6ad70ef9e81408d6a2d3
Moment000 <2578371650@qq.com>
2022-04-21 22:35:05 +0800

    init repo

---
+++ src/main/java/com/ccut/bbsspringboot/BbsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ccut.bbsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbsSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ccut/bbsspringboot/BbsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccut.bbsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
